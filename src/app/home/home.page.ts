f561974a8ec83d06d4f3b1ca28817629dbe04984
Daniel Garnica Sanchez <thedaniel115@gmail.com>
2020-01-09 14:59:47 -0600

    Crear Lista de Tarjetas y boton con alerta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  cardsData = [
+    {
+      title: 'Titulo',
+      subtitle: 'Subtitulo Tarjeta',
+      contentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel repellat eos
+    accusamus excepturi velit odio sunt! Quisquam consequatur repellendus,
+    similique, doloribus cumque voluptatibus debitis reprehenderit labore itaque
+    ex maiores magni!`
+    },
+    {
+      title: 'Titulo 1',
+      subtitle: 'Subtitulo Tarjeta 1',
+      contentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel repellat eos
+    accusamus excepturi velit odio sunt! Quisquam consequatur repellendus,
+    similique, doloribus cumque voluptatibus debitis reprehenderit labore itaque
+    ex maiores magni!`
+    },
+    {
+      title: 'Titulo 2',
+      subtitle: 'Subtitulo Tarjeta 2',
+      contentText: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel repellat eos
+    accusamus excepturi velit odio sunt! Quisquam consequatur repellendus,
+    similique, doloribus cumque voluptatibus debitis reprehenderit labore itaque
+    ex maiores magni!`
+    }
+  ];
+  constructor(public alertController: AlertController) {}
+  async openAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
 
-  constructor() {}
+    await alert.present();
+  }
 
+  onBeerClicked() {
+    console.log('Quiero una chela fria');
+  }
 }
